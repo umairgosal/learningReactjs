ac0897ec28c7dbed9488c24f43e78e27343dcb35
gosal_kali_linux <gosal@kali.gosal>
2024-05-19 04:13:31 +0500

    created a react app and made cards in react

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,23 @@
+import Header from './Header'
+import Card from './Card'
+import Footer from './Footer'
+
+function App() {
+
+  return(
+    <>
+    <Header></Header>
+    <Card></Card>
+    <Card></Card>
+    <Card></Card>
+    <Card></Card> 
+    <Card></Card>
+    <Card></Card>
+    <Card></Card>
+    <Card></Card>
+    <Footer></Footer>
+    </>
+  );
+}
+
+export default App
---
+++ my-react-app/src/Card.jsx
@@ -0,0 +1,15 @@
+import profilePic from './assets/myProfilePic.png'
+
+
+function Card(){
+
+    return(
+        <div className="card">
+            <img className="card-image" src={profilePic} alt="my_profile_pic" />
+            <h2 className="card-name">Umair Gosal</h2>
+            <p className="card-text">I am an undergraduate @ NUST studying Software Engineering</p>
+        </div>
+    );
+}
+
+export default Card
